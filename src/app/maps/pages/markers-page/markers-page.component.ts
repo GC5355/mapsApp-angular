c85157eae61d58b79f40736b0cc0f29c6c08498a
German <gercger5@gmail.com>
2024-08-15 13:11:44 -0300

    Adding markers

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl'
+
+
+interface MarkerAndColor {
+  color: string,
+  marker: Marker
+}
 
 @Component({
   selector: 'maps-markers',
@@ -6,5 +13,71 @@ import { Component } from '@angular/core';
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.5, 40)
+
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'El elemento html no fue encontrado'
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+
+    // ---  Para cambiar los estilos del marcador ---
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Tito Cardoso'
+
+    // const  marker = new Marker({
+    //   //color: 'red'
+    //   element: markerHtml
+    // })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo( this.map )
+  }
+
+
+  createMarker(){
+    if( ! this.map) return
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));      // genera un color hexadecimal de forma aleatoria
+    const lngLat = this.map.getCenter()
+
+    this.addMarker(lngLat, color)
+
+  }
+
+
+  addMarker ( lngLat : LngLat, color : string){
+    if ( ! this.map ) return;
+
+    const marker = new Marker({
+      color: color,
+      draggable: true
+    })
+    .setLngLat( lngLat )
+    .addTo( this.map )
+
+    this.markers.push({
+      marker: marker,
+      color: color
+     });
+  }
+
+  deteleMarker( index: number) {
+    this.markers[index].marker.remove();
+    this.markers.splice( index, 1 );
+  }
+
 
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat } from 'mapbox-gl';
 import { Map } from 'mapbox-gl'
 
 @Component({
@@ -6,55 +7,68 @@ import { Map } from 'mapbox-gl'
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent implements AfterViewInit  {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
 
 
   @ViewChild('map') divMap?: ElementRef;
 
   public zoom: number = 10;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.5, 40)
+
 
   ngAfterViewInit(): void {
 
-    if ( !this.divMap) throw 'El elemento html no fue encontrado'
+    if (!this.divMap) throw 'El elemento html no fue encontrado'
 
-     this.map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
   }
 
-mapListeners(){
-  if (! this.map) throw 'Mapa no inicializado';
 
-  this.map.on('zoom', (ev) => {
-    this.zoom = this.map!.getZoom();
-  })
+                                                                // Listeners del map
+  mapListeners() {
+    if (!this.map) throw 'Mapa no inicializado';
 
-  this.map.on('zoomend', (ev) => {                            // Para establecer un limite de zoom a 18
-    if( this.map!.getZoom() < 18 ) return;
-    this.map!.zoomTo(18)
-  })
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    })
 
-}
+    this.map.on('zoomend', (ev) => {                            // Para establecer un limite de zoom a 18
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18)
+    })
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat } = this.currentLngLat;
+    })
+
+  }
+                                                              // Para limpiar los listeners
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
 
 
 
-zoomIn(){
-  this.map?.zoomIn();
-}
+  zoomIn() {
+    this.map?.zoomIn();
+  }
 
-zoomOut(){
-  this.map?.zoomOut();
-}
+  zoomOut() {
+    this.map?.zoomOut();
+  }
 
-zoomChanged(value : string) {
-  this.zoom = Number( value );
-  this.map?.zoomTo( this.zoom );
-}
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 
 }
